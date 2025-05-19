0b48a92c51bb9f72e6649cdf1c3f388bb0a599b3
Jay989810 <japhethadamu561@gmail.com>
2025-05-19 17:04:26 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ global.location = "ABUJA, Nigeria" //ur location
 
 //new settings 
 global.botname = 'JAY' //ur bot name
-global.ownernumber = '2348161742506' //ur owner number
+global.ownernumber = '2349018541341' //ur owner number
 global.ownername = 'JAY T' //ur owner name
 global.websitex = "https://youtube.com/@elitepro-empire"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
